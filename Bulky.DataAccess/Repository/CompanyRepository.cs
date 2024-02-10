2393ececbf08e34bdda2686b059b60b289c3da44
YevheniKostandov <ykostandov@gmail.com>
2024-02-10 10:13:26 +0100

    update

---
+++ Bulky.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,35 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        public ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company company)
+        {
+            _db.Companies.Update(company);
+            //var compFromDb=_db.Companies.FirstOrDefault(u=>u.Id == company.Id);
+            //if (company!=null)
+            //{
+            //    compFromDb.Id = company.Id;
+            //    compFromDb.PhoneNumber = company.PhoneNumber;
+            //    compFromDb.City = company.City;
+            //    compFromDb.Region = company.Region;
+            //    compFromDb.StreetAddress = company.StreetAddress;
+            //    compFromDb.PostalCode = company.PostalCode;
+            //}
+        }
+    }
+}
