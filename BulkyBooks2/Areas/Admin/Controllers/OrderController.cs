30df6d48bfa5bf190ed7a5f918dcec48f7fe7b1a
YevheniKostandov <ykostandov@gmail.com>
2024-04-07 22:41:18 +0200

    post deployment url settings

--- BulkyBooks2/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBooks2/Areas/Admin/Controllers/OrderController.cs
@@ -123,7 +123,7 @@ namespace BulkyBooks2.Areas.Admin.Controllers
         {
             OrderVM.OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
             OrderVM.OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
-            var domain = "https://localhost:7017/";
+            var domain = Request.Scheme + "://" + Request.Host.Value + "/";
             var options = new Stripe.Checkout.SessionCreateOptions
             {
                 SuccessUrl = domain + $"admin/order/PaymentConfirmation?OrderHeaderId={OrderVM.OrderHeader.Id}",
--- BulkyBooks2/Areas/Customer/Controllers/CartController.cs
+++ BulkyBooks2/Areas/Customer/Controllers/CartController.cs
@@ -145,8 +145,8 @@ namespace BulkyBooks2.Areas.Customer.Controllers
 
 			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
 			{
-                var domain = "https://localhost:7017/";
-				var options = new Stripe.Checkout.SessionCreateOptions
+                var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+                var options = new Stripe.Checkout.SessionCreateOptions
 				{
 					SuccessUrl = domain+$"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
                     CancelUrl = domain+$"customer/cart/index",
